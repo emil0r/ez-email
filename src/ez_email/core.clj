fc5bbd2d9f3525ba75e42d2d70e742b5281d1b9d
Emil Bengtsson <emil@emil0r.com>
2016-09-22 22:58:59 +0200

    working implementation

--- src/ez_email/core.clj
+++ src/ez_email/core.clj
@@ -1,10 +1,11 @@
 (ns ez-email.core
-  (:require [clojure.core.async :as async :refer [go chan close! <! >!]]
+  (:require [clojure.core.async :refer [<! >! chan close! go go-loop sliding-buffer]]
             [clojure.string :as str]
             [postal.core :as postal]))
 
 
-(defonce ^:private channel (atom nil))
+(defonce -channel (atom nil))
+(defonce -provider (atom nil))
 
 (defprotocol IProvider
   (message-sent [provider message result])
@@ -30,33 +31,67 @@
 (defn queue-message
   ([data]
      (go
-       (>! @channel data)))
+       (>! @-channel data)))
   ([to subject body]
      (go
-       (>! @channel {:to to
+       (>! @-channel {:to to
                      :subject subject
                      :body body})))
   ([from to subject body]
      (go
-       (>! @channel {:from from
+       (>! @-channel {:from from
                      :to to
                      :subject subject
                      :body body}))))
 
+(defn shutdown! []
+  (when-let [c (get-in @-provider [:c-in])]
+    (close! c))
+  (when-let [c (get-in @-provider [:c-result])]
+    (close! c))
+  (reset! -channel nil)
+  (reset! -provider nil))
+
 (defn- get-default-opts [opts]
   (let [c-in (or (:c-in opts) (chan))
-        c-result (or (:c-result opts) (chan (async/sliding-buffer 1)))]
-    {:c-in c-in
-     :c-result c-result}))
+        c-result (or (:c-result opts) (chan (sliding-buffer 1)))
+        postal (or (:postal opts)
+                   {:host (:host opts)
+                    :port (:port opts)
+                    :user (:user opts)
+                    :pass (:password opts)
+                    :ssl (:ssl? opts)})]
+    (merge
+     {:init? true
+      :c-in c-in
+      :c-result c-result
+      :postal postal}
+     opts)))
+
+(defn- init [provider opts]
+  (when (true? (:init? opts))
+    (reset! -provider provider)
+    (reset! -channel (:c-in provider))
+    ;; setup channel in
+    (let [c-in (:c-in provider)]
+     (go-loop []
+       (when-let [msg (<! c-in)]
+         (send-message provider msg)
+         (recur))))))
 
 (defn get-provider
   ([opts]
-   (map->PostalProvider (merge (get-default-opts opts)
-                               opts)))
+   (let [opts (get-default-opts opts)
+         provider (map->PostalProvider opts)]
+     (init provider opts)
+     provider))
   ([host user password]
-   (map->PostalProvider (merge (get-default-opts nil)
-                               {:host host :user user :password password})))
+   (let [opts (get-default-opts {:host host :user user :password password})
+         provider (map->PostalProvider opts)]
+     (init provider opts)
+     provider))
   ([host port user password ssl? mime from]
-   (map->PostalProvider (merge (get-default-opts nil)
-                               {:host host :port port :user user :password password
-                                :ssl? ssl? :mime mime :from from}))))
+   (let [opts (get-default-opts {:host host :port port :user user :password password :ssl? ssl? :mime mime :from from})
+         provider (map->PostalProvider opts)]
+     (init provider opts)
+     provider)))
