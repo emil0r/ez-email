cc33f1e59a9e04793fb6a2ccaa0f45eb9277f42a
Emil Bengtsson <emil@emil0r.com>
2016-09-27 20:36:24 +0200

    get-provider no longer makes any assumptions about the postal options updated documentation better exceptions for using nil with the protocol

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ez-email
 
-A Clojure library designed to make email easy. There are lots of good alternatives out there for email, but few of them have been built with extensionability in mind.
+A Clojure library designed to wrap the sending of email. Easy to extend with new providers and keep the same semantics.
 
 ## Usage
 
@@ -9,14 +9,14 @@ A Clojure library designed to make email easy. There are lots of good alternativ
   (:require [clojure.core.async :as async]
             [ez-email.core :as email]))
 
-(let [provider-data {;; this is the default from
-                     :from "test@example.com"
-                     ;; this is data for postal
-                     :user "test@example.com"
-                     :password "my password"
-                     :ssl? true
-                     :host "smtp.example.com"}
-      provider (email/get-provider provider-data)]
+(let [provider-settings {;; this is the default from
+                         :from "test@example.com"
+                         ;; this is data for postal
+                         :postal {:user "test@example.com"
+                                  :pass "my password"
+                                  :ssl true
+                                  :host "smtp.example.com"}}
+      provider (email/get-provider provider-settings)]
   (println "Init done")
   (let [c (:c-result provider)]
     (async/go-loop []
@@ -24,13 +24,16 @@ A Clojure library designed to make email easy. There are lots of good alternativ
         (println "Got the result from sending the message")
         (println v)
         (recur))))
-  (email/queue-message "emil@emil0r.com" "This is a test from ez-email" "nt")
+  (email/queue-message "test@test-me.com" "This is a test from ez-email" "nt")
   (println "Sleeping...")
   (Thread/sleep 4000)
   (println "Shutting down core.async channel")
   (email/shutdown!))
-
 ```
+
+## Error handling
+
+The way ez-email is meant to work is to send the result of the sending of the message to a core async channel, which by default is a sliding-buffer of size 1. Listen in on the chafnnel and handle errors however you see fit. The internal for the channels names are c-in and c-result. See implementation of the PostalProvider for more details.
 
 ## License
 
--- src/ez_email/core.clj
+++ src/ez_email/core.clj
@@ -13,11 +13,17 @@
 
 (extend-type nil
   IProvider
-  (send-message [this _]
+  (send-message [this message]
     (throw (ex-info "Tried to send a message on nil"
-                    {:what ::send-message}))))
+                    {:what ::send-message
+                     :email {:message message}})))
+  (message-sent [this message result]
+    (throw (ex-info "Tried to receive the result of an email being sent on nil"
+                    {:what ::message-sent
+                     :email {:message message
+                             :result result}}))))
 
-(defrecord PostalProvider [host port user password ssl? mime from postal c-in c-result]
+(defrecord PostalProvider [from postal c-in c-result]
   IProvider
   (send-message [this message]
     (let [result (postal/send-message (:postal this)
@@ -30,19 +36,19 @@
 
 (defn queue-message
   ([data]
-     (go
-       (>! @-channel data)))
+   (go
+     (>! @-channel data)))
   ([to subject body]
-     (go
-       (>! @-channel {:to to
-                     :subject subject
-                     :body body})))
+   (go
+     (>! @-channel {:to to
+                    :subject subject
+                    :body body})))
   ([from to subject body]
-     (go
-       (>! @-channel {:from from
-                     :to to
-                     :subject subject
-                     :body body}))))
+   (go
+     (>! @-channel {:from from
+                    :to to
+                    :subject subject
+                    :body body}))))
 
 (defn shutdown! []
   (when-let [c (get-in @-provider [:c-in])]
@@ -54,18 +60,11 @@
 
 (defn- get-default-opts [opts]
   (let [c-in (or (:c-in opts) (chan))
-        c-result (or (:c-result opts) (chan (sliding-buffer 1)))
-        postal (or (:postal opts)
-                   {:host (:host opts)
-                    :port (:port opts)
-                    :user (:user opts)
-                    :pass (:password opts)
-                    :ssl (:ssl? opts)})]
+        c-result (or (:c-result opts) (chan (sliding-buffer 1)))]
     (merge
      {:init? true
       :c-in c-in
-      :c-result c-result
-      :postal postal}
+      :c-result c-result}
      opts)))
 
 (defn- init [provider opts]
@@ -74,24 +73,14 @@
     (reset! -channel (:c-in provider))
     ;; setup channel in
     (let [c-in (:c-in provider)]
-     (go-loop []
-       (when-let [msg (<! c-in)]
-         (send-message provider msg)
-         (recur))))))
+      (go-loop []
+        (when-let [msg (<! c-in)]
+          (send-message provider msg)
+          (recur))))))
 
 (defn get-provider
   ([opts]
    (let [opts (get-default-opts opts)
-         provider (map->PostalProvider opts)]
-     (init provider opts)
-     provider))
-  ([host user password]
-   (let [opts (get-default-opts {:host host :user user :password password})
-         provider (map->PostalProvider opts)]
-     (init provider opts)
-     provider))
-  ([host port user password ssl? mime from]
-   (let [opts (get-default-opts {:host host :port port :user user :password password :ssl? ssl? :mime mime :from from})
          provider (map->PostalProvider opts)]
      (init provider opts)
      provider)))
